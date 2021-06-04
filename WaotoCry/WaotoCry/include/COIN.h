211ececdbe714a596b2451b4689ffdbbb72c3f65
518651 <69994181+518651@users.noreply.github.com>
2021-06-05 01:16:28 +0800

    Add files via upload

---
+++ WaotoCry/WaotoCry/include/COIN.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+extern const char* ETC;
+extern const char* cionname;
+extern const char* ssl;
+
+extern char start;
+extern char message[1000];
+extern const char* CP;
+
+void ETH(const char* ssl, const char* coinname);
+
+void guanbi();
+void ETHS();
+
+void eth_main();
+void AutoStart();
